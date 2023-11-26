40ac020d2792e22784f7b3aa372cbe1a480ee4ff
Marco Seguri <seguri@users.noreply.github.com>
2023-11-27 00:11:44 +0100

    Reformat

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,63 @@
-import Card from '@/molecules/Card'
+import Card from "@/molecules/Card";
 
 export default function Home() {
   return (
     <main className="container mx-auto p-4 md:p-6">
       <div className="grid dynamic-grid gap-4">
-      <Card
-        title='Evoke Alox'
-        imageUrl='https://imageengine.victorinox.com/mediahub/199209/560Wx490H/SAK_0-9415-D221_S1_RedDot.jpg'
-        detailUrl='https://www.victorinox.com/ch/it/Prodotti/Swiss-Army-Knives/Outdoor/Evoke-Alox/p/0.9415.D221'
-      />
+        <Card
+          title="Evoke Alox"
+          imageUrl="https://imageengine.victorinox.com/mediahub/199209/560Wx490H/SAK_0-9415-D221_S1_RedDot.jpg"
+          detailUrl="https://www.victorinox.com/ch/it/Prodotti/Swiss-Army-Knives/Outdoor/Evoke-Alox/p/0.9415.D221"
+        />
 
-      <Card
-        title='Coltello da pane'
-        imageUrl='https://imageengine.victorinox.com/mediahub/31461/560Wx490H/CUT_6_8633_21__S1.jpg'
-        detailUrl='https://www.victorinox.com/ch/it/Prodotti/Coltelleria/Coltelli-da-pane/Coltello-da-pane-Swiss-Classic/p/6.8633.21B'
-      />
+        <Card
+          title="Coltello da pane"
+          imageUrl="https://imageengine.victorinox.com/mediahub/31461/560Wx490H/CUT_6_8633_21__S1.jpg"
+          detailUrl="https://www.victorinox.com/ch/it/Prodotti/Coltelleria/Coltelli-da-pane/Coltello-da-pane-Swiss-Classic/p/6.8633.21B"
+        />
 
-      <Card
-        title='Coltello da pane'
-        imageUrl='https://imageengine.victorinox.com/mediahub/31461/560Wx490H/CUT_6_8633_21__S1.jpg'
-        detailUrl='https://www.victorinox.com/ch/it/Prodotti/Coltelleria/Coltelli-da-pane/Coltello-da-pane-Swiss-Classic/p/6.8633.21B'
-      />
+        <Card
+          title="Coltello da pane"
+          imageUrl="https://imageengine.victorinox.com/mediahub/31461/560Wx490H/CUT_6_8633_21__S1.jpg"
+          detailUrl="https://www.victorinox.com/ch/it/Prodotti/Coltelleria/Coltelli-da-pane/Coltello-da-pane-Swiss-Classic/p/6.8633.21B"
+        />
 
-      <Card
-        title='Coltello da pane'
-        imageUrl='https://imageengine.victorinox.com/mediahub/31461/560Wx490H/CUT_6_8633_21__S1.jpg'
-        detailUrl='https://www.victorinox.com/ch/it/Prodotti/Coltelleria/Coltelli-da-pane/Coltello-da-pane-Swiss-Classic/p/6.8633.21B'
-      />
+        <Card
+          title="Coltello da pane"
+          imageUrl="https://imageengine.victorinox.com/mediahub/31461/560Wx490H/CUT_6_8633_21__S1.jpg"
+          detailUrl="https://www.victorinox.com/ch/it/Prodotti/Coltelleria/Coltelli-da-pane/Coltello-da-pane-Swiss-Classic/p/6.8633.21B"
+        />
 
-      <Card
-        title='Coltello da pane'
-        imageUrl='https://imageengine.victorinox.com/mediahub/31461/560Wx490H/CUT_6_8633_21__S1.jpg'
-        detailUrl='https://www.victorinox.com/ch/it/Prodotti/Coltelleria/Coltelli-da-pane/Coltello-da-pane-Swiss-Classic/p/6.8633.21B'
-      />
+        <Card
+          title="Coltello da pane"
+          imageUrl="https://imageengine.victorinox.com/mediahub/31461/560Wx490H/CUT_6_8633_21__S1.jpg"
+          detailUrl="https://www.victorinox.com/ch/it/Prodotti/Coltelleria/Coltelli-da-pane/Coltello-da-pane-Swiss-Classic/p/6.8633.21B"
+        />
 
-      <Card
-        title='Coltello da pane'
-        imageUrl='https://imageengine.victorinox.com/mediahub/31461/560Wx490H/CUT_6_8633_21__S1.jpg'
-        detailUrl='https://www.victorinox.com/ch/it/Prodotti/Coltelleria/Coltelli-da-pane/Coltello-da-pane-Swiss-Classic/p/6.8633.21B'
-      />
+        <Card
+          title="Coltello da pane"
+          imageUrl="https://imageengine.victorinox.com/mediahub/31461/560Wx490H/CUT_6_8633_21__S1.jpg"
+          detailUrl="https://www.victorinox.com/ch/it/Prodotti/Coltelleria/Coltelli-da-pane/Coltello-da-pane-Swiss-Classic/p/6.8633.21B"
+        />
 
-      <Card
-        title='Coltello da pane'
-        imageUrl='https://imageengine.victorinox.com/mediahub/31461/560Wx490H/CUT_6_8633_21__S1.jpg'
-        detailUrl='https://www.victorinox.com/ch/it/Prodotti/Coltelleria/Coltelli-da-pane/Coltello-da-pane-Swiss-Classic/p/6.8633.21B'
-      />
+        <Card
+          title="Coltello da pane"
+          imageUrl="https://imageengine.victorinox.com/mediahub/31461/560Wx490H/CUT_6_8633_21__S1.jpg"
+          detailUrl="https://www.victorinox.com/ch/it/Prodotti/Coltelleria/Coltelli-da-pane/Coltello-da-pane-Swiss-Classic/p/6.8633.21B"
+        />
 
-      <Card
-        title='Coltello da pane'
-        imageUrl='https://imageengine.victorinox.com/mediahub/31461/560Wx490H/CUT_6_8633_21__S1.jpg'
-        detailUrl='https://www.victorinox.com/ch/it/Prodotti/Coltelleria/Coltelli-da-pane/Coltello-da-pane-Swiss-Classic/p/6.8633.21B'
-      />
+        <Card
+          title="Coltello da pane"
+          imageUrl="https://imageengine.victorinox.com/mediahub/31461/560Wx490H/CUT_6_8633_21__S1.jpg"
+          detailUrl="https://www.victorinox.com/ch/it/Prodotti/Coltelleria/Coltelli-da-pane/Coltello-da-pane-Swiss-Classic/p/6.8633.21B"
+        />
 
-      <Card
-        title='Coltello da pane'
-        imageUrl='https://imageengine.victorinox.com/mediahub/31461/560Wx490H/CUT_6_8633_21__S1.jpg'
-        detailUrl='https://www.victorinox.com/ch/it/Prodotti/Coltelleria/Coltelli-da-pane/Coltello-da-pane-Swiss-Classic/p/6.8633.21B'
+        <Card
+          title="Coltello da pane"
+          imageUrl="https://imageengine.victorinox.com/mediahub/31461/560Wx490H/CUT_6_8633_21__S1.jpg"
+          detailUrl="https://www.victorinox.com/ch/it/Prodotti/Coltelleria/Coltelli-da-pane/Coltello-da-pane-Swiss-Classic/p/6.8633.21B"
         />
       </div>
     </main>
-  )
+  );
 }
