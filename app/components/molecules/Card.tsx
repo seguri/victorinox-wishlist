76872d7717187771efa4ce4c8b2e1ffbde11b75d
Marco Seguri <seguri@users.noreply.github.com>
2023-11-26 19:44:50 +0100

    Add Cards

---
+++ app/components/molecules/Card.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+// Define the type for the props
+type CardProps = {
+  title: string;
+  imageUrl: string;
+  detailUrl: string;
+};
+
+const Card: React.FC<CardProps> = ({ title, imageUrl, detailUrl }) => {
+  return (
+    <div className='card w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2'>
+      <div className='bg-white dark:bg-black text-black dark:text-white p-4 rounded-lg shadow"'>
+      <h2>{title}</h2>
+        <Link href={detailUrl} passHref target="_blank" rel="noopener noreferrer">
+          <Image src={imageUrl} alt={title} width={200} height={200} objectFit="cover" />
+        </Link>
+        </div>
+    </div>
+  );
+};
+
+export default Card;
